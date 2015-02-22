0b834a2cfdb66eeeb527987a623cc8d4a1ef1307
Dominik Zogg <dominik.zogg@gmail.com>
2015-02-22 20:21:32 +0100

    add tests for form label generation

---
+++ tests/Twig/FormLabelExtensionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Saxulum\Tests\Crud\Twig;
+
+use Saxulum\Crud\Twig\FormLabelExtension;
+use Saxulum\Tests\Crud\Data\Form\SampleType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\ResolvedFormTypeFactory;
+
+class FormLabelExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param FormTypeInterface $formType
+     * @param string $expectName
+     * @dataProvider prepareFormLabelProvider
+     */
+    public function testPrepareFormLabel(FormTypeInterface $formType, $expectName)
+    {
+        $formFactory = $this->getFormFactory();
+        $form = $formFactory->create($formType);
+        $formView = $form->createView();
+
+        $extension = new FormLabelExtension();
+
+        $this->assertEquals($expectName, $extension->prepareFormLabel($formView));
+    }
+
+    /**
+     * @return FormFactoryInterface
+     */
+    protected function getFormFactory()
+    {
+        return Forms::createFormFactoryBuilder()
+            ->setResolvedTypeFactory(new ResolvedFormTypeFactory())
+            ->getFormFactory();
+    }
+
+    /**
+     * @return array
+     */
+    public function prepareFormLabelProvider()
+    {
+        return array(
+            array(
+                new SampleType(),
+                'sample.edit.label',
+            ),
+        );
+    }
+}
---
+++ tests/Util/HelperTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Saxulum\Tests\Crud\Util;
+
+use Saxulum\Crud\Util\Helper;
+
+class HelperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param string $input
+     * @param string $expect
+     * @dataProvider camelCaseToUnderscoreProvider
+     */
+    public function testCamelCaseToUnderscore($input, $expect)
+    {
+        $this->assertEquals($expect, Helper::camelCaseToUnderscore($input));
+    }
+
+    /**
+     * @return array
+     */
+    public function camelCaseToUnderscoreProvider()
+    {
+        return array(
+            array(
+                'dies_Ist_Ein_Test',
+                'dies_ist_ein_test',
+            ),
+            array(
+                'Dies_Ist_Ein_Test',
+                'dies_ist_ein_test',
+            ),
+            array(
+                'diesIstEinTest',
+                'dies_ist_ein_test',
+            ),
+            array(
+                'DiesIstEinTest',
+                'dies_ist_ein_test',
+            )
+        );
+    }
+}
